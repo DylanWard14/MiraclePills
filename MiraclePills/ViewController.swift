491a7861a20690310ec1b2d2a4879d6a55e88a2a
Dylan Ward IMac PC <dylan.ward@iinet.net.au>
2018-01-03 23:15:40 +1000

    Added state picker
    Added state picker and code to enable state picker to function correctly. User can now select the state in which they live.

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    let states = ["QLD", "NSW", "TAS", "NT", "VIC", "WA", "ACT"]
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    
+    @IBOutlet weak var stateButton: UIButton!
+    
+    @IBOutlet weak var AcceptButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +29,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateButton.setTitle(states[row], for: UIControlState.normal)
+    }
+    
+    @IBAction func StateButtonPressed(_ sender: Any) {
+        statePicker.isHidden = false
+        AcceptButton.isHidden = false
+    }
+    @IBAction func AcceptButtonPressed(_ sender: Any) {
+        statePicker.isHidden = true
+        AcceptButton.isHidden = true
+    }
 }
 
